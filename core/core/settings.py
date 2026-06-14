c1a8bdf8e2878119d665ac5acff669624099bce0
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-14 10:22:42 +0330

    add home_app,accounts_app,blog_app and edit settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tt_1agjr-$do*#-j5+wl_j_y4lk6omo50o1xpt8*41m-2z%!*n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
 
 
 # Application definition
@@ -37,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my_apps
+    'home_app.apps.HomeAppConfig',
+    'accounts_app.apps.AccountsAppConfig',
+    'blog_app.apps.BlogAppConfig',
+
+    #my_install_apps
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
