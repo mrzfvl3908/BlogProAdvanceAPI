9ddb3d3fa2a2f0e72879dbf58cbc298b1029951f
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-14 10:36:17 +0330

    add models post in models.py in blog_app and admin.py in blog_app

--- core/blog_app/admin.py
+++ core/blog_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog_app.models import Post, Category
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'created_date', 'status']
+    list_filter = ['status', 'created_date']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
--- core/blog_app/models.py
+++ core/blog_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    this is  a class to define posts for blog app
+    """
+    author = models.ForeignKey('accounts_app.Profile', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL)
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, allow_unicode=True, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog_app:post-detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    """
+    this is  a class to define categories for posts in blog app
+    """
+    name = models.CharField(max_length=150)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
