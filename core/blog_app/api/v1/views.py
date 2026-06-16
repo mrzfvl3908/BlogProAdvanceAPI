01763a2922dd5fe9c5a84b3f78f40f439b459c17
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-17 00:13:46 +0330

    add serializer.py in api/v1 and addpostlist , postdetail in views api

--- core/blog_app/api/v1/views.py
+++ core/blog_app/api/v1/views.py
@@ -1,11 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from blog_app.api.v1.serializers import PostSerializer
+from blog_app.models import Post
+
 
 @api_view()
 def post_list(request):
-    return Response('ok')
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
-def post_detail(request,id):
-    return Response(id)
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
