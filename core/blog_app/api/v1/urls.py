1802967cce902b72c75fe4e8b333e339de5f0c9c
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-17 12:16:54 +0330

    add modelviewset in views and router in urls.py in api/v1

--- core/blog_app/api/v1/urls.py
+++ core/blog_app/api/v1/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+
 app_name = 'api-v1'
-urlpatterns = [
-    path('post/', views.PostList.as_view(), name='post-list'),
-    path('post/<int:id>/', views.PostDetail.as_view(), name='post-detail'),
-]
+
+router = DefaultRouter()
+
+router.register('post', views.PostListModelViewSet, basename='post')
+
+urlpatterns = router.urls
+
+
+
+
+# urlpatterns = [
+#     path('post/', views.PostList.as_view(), name='post-list'),
+#     path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+# ]
--- core/blog_app/api/v1/views.py
+++ core/blog_app/api/v1/views.py
@@ -1,5 +1,6 @@
 from django.core.serializers import serialize
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -7,7 +8,7 @@ from rest_framework.views import APIView
 from blog_app.api.v1.serializers import PostSerializer
 from blog_app.models import Post
 
-
+"""Example DBV"""
 # @api_view()
 # def post_list(request):
 #     posts = Post.objects.filter(status=True).order_by('-created_date')
@@ -20,48 +21,68 @@ from blog_app.models import Post
 #     serializer = PostSerializer(post)
 #     return Response(serializer.data)
 
-class PostList(APIView):
-    """
-    getting a list of post and creating new post
-    """
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostSerializer #=> برای فرم پست کردن حرفه ای میشه
 
-    def get(self, request):
-        """
-        retrieving a list of posts
-        """
-        posts = Post.objects.filter(status=True)
-        serializer = self.serializer_class(posts, many=True)
-        return Response(serializer.data)
+"""Example APIView"""
+# class PostList(APIView):
+#     """
+#     getting a list of post and creating new post
+#     """
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = PostSerializer #=> برای فرم پست کردن حرفه ای میشه
+#
+#     def get(self, request):
+#         """
+#         retrieving a list of posts
+#         """
+#         posts = Post.objects.filter(status=True)
+#         serializer = self.serializer_class(posts, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         """
+#         creating a new post
+#         """
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def post(self, request):
-        """
-        creating a new post
-        """
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+# class PostDetail(APIView):
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     serializer_class = PostSerializer
+#
+#     def get(self, request, id):
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         serializer = self.serializer_class(post)
+#         return Response(serializer.data)
+#
+#     def put(self, request, id):
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         serializer = self.serializer_class(post, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#
+#     def delete(self, request, id):
+#         post = get_object_or_404(Post, pk=id, status=True)
+#         post.delete()
+#         return Response({"detail": "successfully delete post"}, status=status.HTTP_204_NO_CONTENT)
 
 
-class PostDetail(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+"""Example GenericView"""
+# class PostList(ListCreateAPIView):
+#     permission_classes = (IsAuthenticatedOrReadOnly,)
+#     serializer_class = PostSerializer
+#     queryset = Post.objects.filter(status=True)
+#
+# class PostDetail(RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthenticatedOrReadOnly,)
+#     serializer_class = PostSerializer
+#     queryset = Post.objects.filter(status=True)
+
+
+"""Example ModelViewSet"""
+class PostListModelViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = PostSerializer
-
-    def get(self, request, id):
-        post = get_object_or_404(Post, pk=id, status=True)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        post = get_object_or_404(Post, pk=id, status=True)
-        serializer = self.serializer_class(post, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, id):
-        post = get_object_or_404(Post, pk=id, status=True)
-        post.delete()
-        return Response({"detail":"successfully delete post"},status=status.HTTP_204_NO_CONTENT)
+    queryset = Post.objects.filter(status=True)
