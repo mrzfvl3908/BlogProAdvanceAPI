3d8341a417f9ddb53810e101c324c711e6a8378c
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-18 23:59:02 +0330

    add other filters in views.py in api

--- core/blog_app/api/v1/views.py
+++ core/blog_app/api/v1/views.py
@@ -100,3 +100,29 @@ class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
+
+#=> برای ساختن فیلترهای اختصاصی و کاستوم شده
+# from rest_framework import generics
+# from django_filters import rest_framework as filters
+# from myapp import Product
+#
+#
+# class ProductFilter(filters.FilterSet):
+#     min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+#     max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+#
+#     class Meta:
+#         model = Product
+#         fields = ['category', 'in_stock']
+#
+#
+# class ProductList(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     filter_backends = (filters.DjangoFilterBackend,)
+#     filterset_class = ProductFilter
+
+
+
