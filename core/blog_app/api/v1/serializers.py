ff21f5b50752a9c835d47dd3e655dd00fac121df
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-18 16:14:08 +0330

    update to_representation in serializers.py in api/v1 => blog_app

--- core/blog_app/api/v1/serializers.py
+++ core/blog_app/api/v1/serializers.py
@@ -14,6 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
     relative_url = serializers.URLField(source='get_absolute_api_url',
                                         read_only=True)  # => توابع وابسته به مدل و فانکشن از مدل
     absolute_url = serializers.SerializerMethodField()  # => برای توابه ئابسته بع داخل سریالایزر اگه درخواست داریم باید از درو همین سریالایزر فانکشن بنویسم اگر ن از مدل میشه
+
     # absolute_url = serializers.SerializerMethodField(method_name='get_abs_url') => get_absolute_url برای تغییر نام و استفاده از متد نام به جای
     # category = CategorySerializer() => گرفتن موارد کتگوری
 
@@ -23,11 +24,19 @@ class PostSerializer(serializers.ModelSerializer):
                   'relative_url', 'absolute_url']
         # read_only_fields = ['content']
 
-    def get_absolute_url(self, obj): #=> برای توابع درونی سریالایزر
+    def get_absolute_url(self, obj):  # => برای توابع درونی سریالایزر
         request = self.context.get('request')
         return request.build_absolute_uri(
             obj.get_absolute_api_url()
         )
 
-    def to_representation(self, instance):
-        return super().to_representation(instance)
+    def to_representation(self, instance): # => برای گرفتن ابجکت و حذف در post_detail و نمایش در لیست پست ها
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep
