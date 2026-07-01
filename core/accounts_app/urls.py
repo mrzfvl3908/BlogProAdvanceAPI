3582c09782404b2cae678006d5b8135529cf3a1f
Mohammadreza <mrzfvl3908@gmail.com>
2026-07-01 11:14:31 +0330

    add api in accounts_app

--- core/accounts_app/urls.py
+++ core/accounts_app/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 app_name = "accounts_app"
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("register/", RegisterPage.as_view(), name="register"),
-
-    # logout
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path('api/v1/', include('accounts_app.api.v1.urls')),
 ]
