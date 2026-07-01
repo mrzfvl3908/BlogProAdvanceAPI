10ab04a22c1c0783f7d3492b8995b3d95dda6a38
Mohammadreza <mrzfvl3908@gmail.com>
2026-07-01 16:30:42 +0330

    Update urls.py

--- core/accounts_app/api/v1/urls.py
+++ core/accounts_app/api/v1/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api-v1'
 urlpatterns = [
-    #registration
+    # registration
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
-    #change password
-    #reset password
-    #login token
-    #login jwt
+    path('token/login', ObtainAuthToken.as_view(), name='token-login'),
+    # change password
+    # reset password
+    # login token
+    # login jwt
 ]
